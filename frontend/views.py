a3a1272e2242309d6e9a75579e645102944e7651
Gift <giftjeremiah001@gmail.com>
2023-04-22 18:29:39 +0100

    feature: Created frontend login page

---
+++ frontend/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from rest_framework import viewsets
+from rest_framework.response import Response
+from django.views.generic import TemplateView
+
+class LoginView(TemplateView):
+    template_name = 'frontend/login.html'
